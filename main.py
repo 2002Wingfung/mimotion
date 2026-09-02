70919bb7bef55cd341fb1bf768ee5babb0ccfd38
2002Wingfung <1039377283@qq.com>
2026-09-02 20:01:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -135,8 +135,8 @@ class MiMotion():
         max_ratio = math.ceil(hour / 3)
         # min_step = int(3500 * min_ratio * self.factor)
         # max_step = int(3500 * max_ratio * self.factor)
-        min_step = 7000
-        max_step = 7800
+        min_step = 6700
+        max_step = 7200
         return min_step, max_step
 
     def run(self):
@@ -147,7 +147,7 @@ class MiMotion():
                 min_step, max_step = max_step, min_step
         except Exception as e:
             print(f'步数范围初始化失败: {e}，使用默认值')
-            min_step, max_step = 7000, 7800
+            min_step, max_step = 6700, 7200
 
         step = str(random.randint(min_step, max_step))
 
